735909787cc31ed35a45b1c52bc0167cb08ab329
Mikhail Chechnev <mim_@mail.ru>
2019-03-08 20:26:25 +0300

    Удалил объекты БД учитываемые в СХС

--- db/EXSRNLIST.sql
+++
@@ -1,9 +0,0 @@
-create global temporary table EXSRNLIST
-(
-  RN                        number(17),
-  IDENT                     number(17) not null,
-  DOCUMENT                  number(17) not null,
-  constraint C_EXSRNLIST_PK primary key(RN),
-  constraint C_EXSRNLIST_UK unique(IDENT, DOCUMENT)
-)
-on commit preserve rows;
